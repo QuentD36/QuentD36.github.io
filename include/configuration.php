75a13e1aa87a6cc3cb135f9d19cc1fc6e77da5cb
QuentD36 <82884437+QuentD36@users.noreply.github.com>
2022-07-09 17:08:52 +0200

    reset

--- include/configuration.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once('settings.php');
-require_once('model.php');
-require_once('autoloader.php');
-require_once('libs/Smarty.class.php');
--- include/settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('NAME', 'root');
-define('PWD', '');
-define('BASE', 'wild_code_challenge');
-define('SERVER', 'localhost');
